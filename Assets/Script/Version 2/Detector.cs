cf41d2f75b62b7debf6a738362418e64819702ee
LauncherMingS <4minie015714@gmail.com>
2025-03-30 02:51:51 +0800

    [Feature]: Add Detector for closest target detection
    - Implement Detector.cs to find the closest target within range
    - Integrate Detector into Unlit.cs for movement decision

---
+++ Assets/Script/Version 2/Detector.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace Assets.Version2
+{
+    public class Detector : MonoBehaviour
+    {
+        private static Collider[] detectedColliders = new Collider[64];
+
+        [SerializeField] private float m_detectRange = 3f;
+        [SerializeField] private int m_targetLayerMask = 128;
+
+        public Transform DetectClosestTarget()
+        {
+            int t_detectLength = Physics.OverlapSphereNonAlloc(transform.position
+                , m_detectRange, detectedColliders, m_targetLayerMask, QueryTriggerInteraction.Ignore);
+            Transform t_detectedTarget;
+            Transform t_target = null;
+            float t_distance;
+            float t_targetDistance = float.MaxValue;
+
+            for (int i = 0;i < t_detectLength;i++)
+            {
+                t_detectedTarget = detectedColliders[i].transform;
+                t_distance = (transform.position - t_detectedTarget.position).sqrMagnitude;
+                if (t_targetDistance > t_distance)
+                {
+                    t_targetDistance = t_distance;
+                    t_target = t_detectedTarget;
+                }
+            }
+
+            return t_target;
+        }
+    }
+}
--- Assets/Script/Version 2/Unit.cs
+++ Assets/Script/Version 2/Unit.cs
@@ -5,7 +5,18 @@ namespace Assets.Version2
     public class Unit : MonoBehaviour
     {
         [Header("Component Reference")]
+        [SerializeField] private Detector m_detector;
+
         [SerializeField] private Movement m_movement;
+        [SerializeField] private Vector3 m_defaultPosition;
+
+        private void Update()
+        {
+            Transform t_target = m_detector.DetectClosestTarget();
+            Vector3 t_targetPosition = t_target ? t_target.position : m_defaultPosition;
+
+            m_movement.MoveTo(t_targetPosition);
+        }
 
         private void Awake()
         {
