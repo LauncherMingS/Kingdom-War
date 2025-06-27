7bb54ac3a26a4ecbad04145441f24e42e8423042
LauncherMingS <4minie015714@gmail.com>
2025-06-28 03:06:34 +0800

    [Feature] Add two line up functions
    - First function LineUp() use to line up all units when player press the defense button to issue defense command.
    Distribute the units evenly on a line(z axis), called a row, with a maximum of four units in a row, and then arrange them from the center to the nexus.
    - Second function LineUpLastRow() only line up the units of last row.
    This function is used when recruiting new unit to avoid recalculating the position of previously calculated units.
    - Add three Headers

--- Assets/Script/Version 2/Controller.cs
+++ Assets/Script/Version 2/Controller.cs
@@ -7,16 +7,23 @@ namespace Assets.Version2
     {
         [SerializeField] private Command m_currentCommand = Command.None;
 
+        [Header("Position Data")]
         [SerializeField] private Vector3 m_currentIndicationPosition;
         [SerializeField] private Vector3 m_enemyBasePosition;
-        [SerializeField] private Vector3 m_defendPosition;
         [SerializeField] private Vector3 m_retreatPosition;
 
-        //Test
+        [Header("Line Up Data")]
+        [SerializeField] private float m_beginX = -10f;
+        [SerializeField] private float m_commonDifferenceX = -3f;
+        [SerializeField] private float m_totalLengthZ = 12f;
+        [SerializeField] private int m_maxUnitPerRow = 4;
+
+        [Header("Unit List")]
         [SerializeField] private List<Unit> m_units;
 
         public Command CurrentCommand => m_currentCommand;
 
+        //Button Listener
         public void SwitchCommand(int newCommandFlag)
         {
             if (newCommandFlag > 3)
@@ -41,8 +48,8 @@ namespace Assets.Version2
                     m_currentIndicationPosition = m_enemyBasePosition;
                     break;
                 case Command.Defend:
-                    m_currentIndicationPosition = m_defendPosition;
-                    break;
+                    LineUp();
+                    return;
                 case Command.Retreat:
                     m_currentIndicationPosition = m_retreatPosition;
                     break;
@@ -57,10 +64,73 @@ namespace Assets.Version2
             }
         }
 
+        public void LineUp()
+        {
+            if (m_units.Count == 0)
+            {
+                return;
+            }
+
+            int t_currentRow = 0;
+            int t_remainUnitCount = m_units.Count;
+            Vector3 t_unitPosition = Vector3.zero;
+            while (t_remainUnitCount > 0)
+            {
+                int t_unitCountThisRow = (t_remainUnitCount > m_maxUnitPerRow) 
+                    ? m_maxUnitPerRow : t_remainUnitCount;
+
+                int t_rowStartIndex = t_currentRow * m_maxUnitPerRow;
+                t_unitPosition.x = t_currentRow * m_commonDifferenceX + m_beginX;
+                for (int i = 0; i < t_unitCountThisRow; i++)
+                {
+                    t_unitPosition.z = m_totalLengthZ * (i + 1) / (t_unitCountThisRow + 1);
+
+                    int t_unitIndex = t_rowStartIndex + i;
+                    if (t_unitIndex < m_units.Count)
+                    {
+                        m_units[t_unitIndex].SetDefaultPosition = t_unitPosition;
+                    }
+                }
+
+                t_remainUnitCount -= t_unitCountThisRow;
+                t_currentRow++;
+            }
+        }
+
+        public void LineUpLastRow()
+        {
+            if (m_units.Count == 0)
+            {
+                return;
+            }
+
+            int t_lastRowIndex = m_units.Count / m_maxUnitPerRow;
+            int t_unitCountLastRow = m_units.Count % m_maxUnitPerRow;
+            if (t_unitCountLastRow == 0)
+            {
+                t_lastRowIndex--;
+                t_unitCountLastRow = m_maxUnitPerRow;
+            }
+
+            int t_rowStartIndex = t_lastRowIndex * m_maxUnitPerRow;
+            Vector3 t_unitPosition = Vector3.zero;
+            t_unitPosition.x = t_lastRowIndex * m_commonDifferenceX + m_beginX;
+            for (int i = 0;i < t_unitCountLastRow;i++)
+            {
+                t_unitPosition.z = m_totalLengthZ * (i + 1) / (t_unitCountLastRow + 1);
+
+                int t_unitIndex = t_rowStartIndex + i;
+                if (t_unitIndex < m_units.Count)
+                {
+                    m_units[t_unitIndex].SetDefaultPosition = t_unitPosition;
+                }
+            }
+        }
+
         public void AddUnit(Unit unit)
         {
             m_units.Add(unit);
-            unit.SetDefaultPosition = m_currentIndicationPosition;
+            LineUpLastRow();
         }
 
         private void Start()
