0f845066f2ec56c3d900c4bcda8e999b30192764
LauncherMingS <4minie015714@gmail.com>
2025-07-04 23:07:12 +0800

    [Feature] In response to the rework of character animation, refactor and add new function
    - Character is now rendered by each component independently, m_spriteRenderer needs to change to array
    , and HurtFlash also needs to be refactored
    - Add flip animation playable, and refactor the Face()
    - Add BlendAnimation() and refacor SwithcAnimation() to implement animation switching transition

--- Assets/Script/Version 2/Component/View.cs
+++ Assets/Script/Version 2/Component/View.cs
@@ -8,18 +8,27 @@ namespace Assets.Version2
     public class View : MonoBehaviour
     {
         [Header("Sprite Renderer")]
-        [SerializeField] private SpriteRenderer m_spriteRenderer;
+        [SerializeField] private SpriteRenderer[] m_spriteRenderer;
         [SerializeField] private float m_hurtFlashDuration = 0.35f;
         [SerializeField] private IEnumerator m_coroutine;
         [SerializeField] private bool m_coroutineStatus;
         [SerializeField] private bool m_defaultFlipX;
+        [SerializeField] private bool m_currentFlipX = false;
 
         [Header("Animation")]
         [SerializeField] private PlayableGraph m_playableGraph;
         [SerializeField] private AnimationMixerPlayable m_mixerPlayable;
+        [SerializeField] private AnimationMixerPlayable m_flipMixerPlayable;
         [SerializeField] private AnimationClip m_attackClip;
         [SerializeField] private AnimationClip m_idleClip;
         [SerializeField] private AnimationClip m_moveClip;
+        [SerializeField] private AnimationClip m_flipXTrue;
+        [SerializeField] private AnimationClip m_flipXFalse;
+        [SerializeField] private float m_duration = 0.5f;
+        [SerializeField] private int m_currentState;
+        [SerializeField] private int m_nextState;
+        [SerializeField] private bool m_blendStatus;
+        [SerializeField] private IEnumerator m_blendAnimation;
 
         public void Face(float positionX)
         {
@@ -33,7 +42,18 @@ namespace Assets.Version2
                 t_face = false;
             }
 
-            m_spriteRenderer.flipX = t_face;
+            if (m_currentFlipX == t_face)
+            {
+                return;
+            }
+
+            int t_input = (t_face) ? 1 : 0;
+            m_flipMixerPlayable.SetInputWeight(t_input ^ 1, 0f);
+            m_flipMixerPlayable.SetInputWeight(t_input, 1f);
+            m_flipMixerPlayable.GetInput(t_input).SetTime(0d);
+            m_flipMixerPlayable.GetInput(t_input).SetDone(false);
+
+            m_currentFlipX = t_face;
         }
 
         public void HurtVisualEffect(float point)
@@ -51,11 +71,40 @@ namespace Assets.Version2
         {
             m_coroutineStatus = true;
 
-            m_spriteRenderer.color = Color.red;
+            int t_length = m_spriteRenderer.Length;
+            for (int i = 0; i < t_length; i++)
+            {
+                m_spriteRenderer[i].color = Color.red;
+            }
             yield return new WaitForSeconds(m_hurtFlashDuration);
-            m_spriteRenderer.color = Color.white;
+            for (int i = 0; i < t_length; i++)
+            {
+                m_spriteRenderer[i].color = Color.white;
+            }
 
             m_coroutineStatus = false;
+        }
+
+        private IEnumerator BlendAnimation()
+        {
+            m_blendStatus = true;
+
+            float t_time = 0f;
+
+            while(t_time < m_duration)
+            {
+                t_time += Time.deltaTime;
+                float t_weight = t_time / m_duration;
+
+                m_mixerPlayable.SetInputWeight(m_currentState, 1f - t_weight);
+                m_mixerPlayable.SetInputWeight(m_nextState, t_weight);
+                yield return null;
+            }
+
+            m_mixerPlayable.SetInputWeight(m_currentState, 0f);
+            m_mixerPlayable.SetInputWeight(m_nextState, 1f);
+
+            m_blendStatus = false;
         }
 
         public bool AnimationIsDone(int unitState)
@@ -65,8 +114,22 @@ namespace Assets.Version2
 
         public void SwitchAnimation(int newState, int originState)
         {
-            m_mixerPlayable.SetInputWeight(originState, 0f);
-            m_mixerPlayable.SetInputWeight(newState, 1f);
+            if (m_nextState == newState)
+            {
+                return;
+            }
+
+            if (m_blendStatus)
+            {
+                StopCoroutine(m_blendAnimation);
+                m_mixerPlayable.SetInputWeight(m_currentState, 0f);
+                m_mixerPlayable.SetInputWeight(m_nextState, 1f);
+            }
+
+            m_currentState = originState;
+            m_nextState = newState;
+            m_blendAnimation = BlendAnimation();
+            StartCoroutine(m_blendAnimation);
         }
 
         public void ResetAnimation(int currentState)
@@ -85,19 +148,35 @@ namespace Assets.Version2
             m_playableGraph.SetTimeUpdateMode(DirectorUpdateMode.GameTime);
 
             AnimationPlayableOutput t_playableOutput = AnimationPlayableOutput.Create(m_playableGraph, "Infantry Output", t_animator);
-            
+
             m_mixerPlayable = AnimationMixerPlayable.Create(m_playableGraph, 3);
             t_playableOutput.SetSourcePlayable(m_mixerPlayable);
 
             AnimationClipPlayable t_idlePlayable = AnimationClipPlayable.Create(m_playableGraph, m_idleClip);
             AnimationClipPlayable t_movePlayable = AnimationClipPlayable.Create(m_playableGraph, m_moveClip);
             AnimationClipPlayable t_attackPlayable = AnimationClipPlayable.Create(m_playableGraph, m_attackClip);
-            t_movePlayable.SetSpeed(6d);
+            t_idlePlayable.SetSpeed(0.5d);
+            //t_movePlayable.SetSpeed(0.5d);
             t_attackPlayable.SetDuration((double)m_attackClip.length);
-            t_attackPlayable.SetSpeed(1.5d);
+            //t_movePlayable.SetSpeed(6d);
+            //t_attackPlayable.SetDuration((double)m_attackClip.length);
+            //t_attackPlayable.SetSpeed(1.5d);
             m_playableGraph.Connect(t_idlePlayable, 0, m_mixerPlayable, (int)SwordMan.UnitState.Idle);
             m_playableGraph.Connect(t_movePlayable, 0, m_mixerPlayable, (int)SwordMan.UnitState.Move);
             m_playableGraph.Connect(t_attackPlayable, 0, m_mixerPlayable, (int)SwordMan.UnitState.Attack);
+
+
+            AnimationClipPlayable t_flipXTrue = AnimationClipPlayable.Create(m_playableGraph, m_flipXTrue);
+            AnimationClipPlayable t_flipXFalse = AnimationClipPlayable.Create(m_playableGraph, m_flipXFalse);
+            t_flipXTrue.SetSpeed(2f);
+            t_flipXFalse.SetSpeed(2f);
+
+            m_flipMixerPlayable = AnimationMixerPlayable.Create(m_playableGraph, 2);
+            m_playableGraph.Connect(t_flipXFalse, 0, m_flipMixerPlayable, 0);
+            m_playableGraph.Connect(t_flipXTrue, 0, m_flipMixerPlayable, 1);
+
+            AnimationPlayableOutput t_flipOutput = AnimationPlayableOutput.Create(m_playableGraph, "Infantry Output", t_animator);
+            t_flipOutput.SetSourcePlayable(m_flipMixerPlayable);
 
             SwitchAnimation(0, 1);
 
@@ -112,6 +191,7 @@ namespace Assets.Version2
         public void Uninitialize()
         {
             GetComponent<Health>().OnHurt -= HurtVisualEffect;
+            m_playableGraph.Destroy();
         }
     }
 }
